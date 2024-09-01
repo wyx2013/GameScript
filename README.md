714ab95fec35b0018dba3723aeffccf899deaba9
Your Name <you@example.com>
2024-09-01 15:19:09 +0800

    commit 2

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # GameScripts 语言
 ## 一个用于编制游戏的语言，配套官方编辑器，可用简洁的语言制作出功能强大的游戏
 ###### 请访问 [官网]() 查看更多详细信息
+[github](https://github.com)
+
+password:wyxwyx1122_
--- gui/ed.py
+++ gui/ed.py
@@ -1,1 +1,14 @@
 #the ide file for gameScript
+#include<asmp>
+#cas asmp.s.define
+#.Bs bds
+import tkinter,time,os,sys
+import mingw
+from colorama import init,Back,Style
+init(autoreset=True)
+def pr(c:any,*string:str):
+    pass
+def main():
+    pass
+if __name__=="__main__":
+    main()
