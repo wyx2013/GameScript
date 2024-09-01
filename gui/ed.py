f76e76fbbc49f00e808eae4f5b62a5dcba38da8b
Your Name <you@example.com>
2024-09-01 15:53:18 +0800

    commit6

--- gui/ed.py
+++ gui/ed.py
@@ -1,4 +1,4 @@
-#the ide file for gameScript
+"the ide file for gameScript"
 #include<asmp>
 #cas asmp.s.define
 #.Bs bds
@@ -6,7 +6,13 @@ import tkinter,time,os,sys
 import mingw
 from colorama import init,Back,Style
 init(autoreset=True)
-def main():
-    pass
+print(Back.RED+"start!")
+print(Style.BRIGHT+Back.LIGHTBLACK_EX+"init...")
+root=tkinter.Tk()
+root.title("GameScripts IDE")
+root.geometry("900x700")
+root.minsize(900,700)
+def main():pass
+    
 if __name__=="__main__":
     main()
